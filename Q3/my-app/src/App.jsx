511406afc6de7287956da7ab3f5ed2b73705e165
Mateus Melo <mateus@mateusmelo.com>
2025-07-09 22:08:40 -0300

    Solves Q3 WIP

--- Q3/my-app/src/App.jsx
+++ Q3/my-app/src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Stack, Box, CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiStack: {
+      defaultProps: {
+        useFlexGap: true,
+      },
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "100vh",
+          }}
+        >
+          <Stack
+            sx={{
+              height: "100%",
+              width: "100%",
+              alignItems: "stretch",
+              gap: 2,
+            }}
+          >
+            <Box bgcolor="#54D4EB" sx={{ flexGrow: 1 }}>
+              Header
+            </Box>
+            <Box sx={{ flexGrow: 5 }}>
+              <Stack
+                direction="row"
+                sx={{
+                  alignItems: "stretch",
+                }}
+              >
+                <Box sx={{ flexGrow: 1, flexBasis: 1 }}>
+                  <Stack>
+                    <Box bgcolor="#D7C9E3" sx={{ flexGrow: 1 }}>
+                      Hero
+                    </Box>
+                    <Box bgcolor="#9FC363">Sidebar</Box>
+                  </Stack>
+                </Box>
+                <Box sx={{ flexGrow: 2, flexBasis: 2 }}>
+                  <Stack
+                    sx={{
+                      alignItems: "stretch",
+                    }}
+                  >
+                    <Box bgcolor="#F5C632">Main Content</Box>
+                    <Box bgcolor="#898989">Extra Content</Box>
+                  </Stack>
+                </Box>
+              </Stack>
+            </Box>
+            <Box sx={{ flexGrow: 2 }}>
+              <Stack direction="row">
+                <Box bgcolor="#2AB676" sx={{ flexGrow: 2 }}>
+                  Related Images
+                </Box>
+                <Box bgcolor="#F3CCDE" sx={{ flexGrow: 1 }}>
+                  Related Posts
+                </Box>
+              </Stack>
+            </Box>
+            <Box bgcolor="#FEA500" sx={{ flexGrow: 1 }}>
+              Footer
+            </Box>
+          </Stack>
+        </Box>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
