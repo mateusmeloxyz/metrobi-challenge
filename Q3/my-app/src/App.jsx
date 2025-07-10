a0b9f66507c04d2ff1979f9a19c1c4142a6eba92
Mateus Melo <mateus@mateusmelo.com>
2025-07-10 16:14:56 -0300

    Fix flex grow across all boxes

--- Q3/my-app/src/App.jsx
+++ Q3/my-app/src/App.jsx
@@ -34,35 +34,63 @@ function App() {
             <Box bgcolor="#54D4EB" sx={{ flexGrow: 1 }}>
               Header
             </Box>
-            <Box sx={{ flexGrow: 5 }}>
+            <Box
+              sx={{
+                flexGrow: 7,
+              }}
+            >
               <Stack
                 direction="row"
                 sx={{
+                  height: "100%",
+                  width: "100%",
+                  gap: 2,
                   alignItems: "stretch",
                 }}
               >
                 <Box sx={{ flexGrow: 1, flexBasis: 1 }}>
-                  <Stack>
+                  <Stack
+                    sx={{
+                      height: "100%",
+                      gap: 2,
+                      alignItems: "stretch",
+                    }}
+                  >
                     <Box bgcolor="#D7C9E3" sx={{ flexGrow: 1 }}>
                       Hero
                     </Box>
-                    <Box bgcolor="#9FC363">Sidebar</Box>
+                    <Box bgcolor="#9FC363" sx={{ flexGrow: 2 }}>
+                      Sidebar
+                    </Box>
                   </Stack>
                 </Box>
                 <Box sx={{ flexGrow: 2, flexBasis: 2 }}>
                   <Stack
                     sx={{
+                      height: "100%",
+                      gap: 2,
                       alignItems: "stretch",
                     }}
                   >
-                    <Box bgcolor="#F5C632">Main Content</Box>
-                    <Box bgcolor="#898989">Extra Content</Box>
+                    <Box bgcolor="#F5C632" sx={{ flexGrow: 2 }}>
+                      Main Content
+                    </Box>
+                    <Box bgcolor="#898989" sx={{ flexGrow: 1 }}>
+                      Extra Content
+                    </Box>
                   </Stack>
                 </Box>
               </Stack>
             </Box>
             <Box sx={{ flexGrow: 2 }}>
-              <Stack direction="row">
+              <Stack
+                sx={{
+                  height: "100%",
+                  gap: 2,
+                  alignItems: "stretch",
+                  flexDirection: "row",
+                }}
+              >
                 <Box bgcolor="#2AB676" sx={{ flexGrow: 2 }}>
                   Related Images
                 </Box>
