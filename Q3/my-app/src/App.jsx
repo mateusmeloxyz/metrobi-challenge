c79fdad9599eb6c08f4aedc62a21f67a979d0c63
Mateus Melo <mateus@mateusmelo.com>
2025-07-14 09:24:26 -0300

    Fix max width and centers the tiles

--- Q3/my-app/src/App.jsx
+++ Q3/my-app/src/App.jsx
@@ -18,101 +18,111 @@ function App() {
     <>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-        <Box
+        <Stack
           sx={{
             width: "100%",
-            height: "100vh",
+            height: "100%",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          <Stack
+          <Box
             sx={{
+              width: 800,
               height: "100%",
-              width: "100%",
-              alignItems: "stretch",
-              gap: 2,
             }}
           >
-            <ColorBox bgcolor="#54D4EB" sx={{ flexGrow: 1 }} title="Header" />
-            <Box
+            <Stack
               sx={{
-                flexGrow: 7,
+                height: "100%",
+                width: "100%",
+                alignItems: "stretch",
+                gap: 2,
               }}
             >
-              <Stack
-                direction="row"
+              <ColorBox bgcolor="#54D4EB" sx={{ flexGrow: 1 }} title="Header" />
+              <Box
                 sx={{
-                  height: "100%",
-                  width: "100%",
-                  gap: 2,
-                  alignItems: "stretch",
+                  flexGrow: 7,
                 }}
               >
-                <Box sx={{ flexGrow: 1, flexBasis: 1 }}>
-                  <Stack
-                    sx={{
-                      height: "100%",
-                      gap: 2,
-                      alignItems: "stretch",
-                    }}
-                  >
-                    <ColorBox
-                      bgcolor="#D7C9E3"
-                      sx={{ flexGrow: 1 }}
-                      title="Hero"
-                    />
-                    <ColorBox
-                      bgcolor="#9FC363"
-                      sx={{ flexGrow: 2 }}
-                      title="Sidebar"
-                    />
-                  </Stack>
-                </Box>
-                <Box sx={{ flexGrow: 2, flexBasis: 2 }}>
-                  <Stack
-                    sx={{
-                      height: "100%",
-                      gap: 2,
-                      alignItems: "stretch",
-                    }}
-                  >
-                    <ColorBox
-                      bgcolor="#F5C632"
-                      sx={{ flexGrow: 2 }}
-                      title="Main Content"
-                    />
-                    <ColorBox
-                      bgcolor="#898989"
-                      sx={{ flexGrow: 1 }}
-                      title="Extra Content"
-                    />
-                  </Stack>
-                </Box>
-              </Stack>
-            </Box>
-            <Box sx={{ flexGrow: 2 }}>
-              <Stack
-                sx={{
-                  height: "100%",
-                  gap: 2,
-                  alignItems: "stretch",
-                  flexDirection: "row",
-                }}
-              >
-                <ColorBox
-                  bgcolor="#2AB676"
-                  sx={{ flexGrow: 2 }}
-                  title="Related Images"
-                />
-                <ColorBox
-                  bgcolor="#F3CCDE"
-                  sx={{ flexGrow: 1 }}
-                  title="Related Posts"
-                />
-              </Stack>
-            </Box>
-            <ColorBox bgcolor="#FEA500" sx={{ flexGrow: 1 }} title="Footer" />
-          </Stack>
-        </Box>
+                <Stack
+                  direction="row"
+                  sx={{
+                    height: "100%",
+                    width: "100%",
+                    gap: 2,
+                    alignItems: "stretch",
+                  }}
+                >
+                  <Box sx={{ flexGrow: 1, flexBasis: 1 }}>
+                    <Stack
+                      sx={{
+                        height: "100%",
+                        gap: 2,
+                        alignItems: "stretch",
+                      }}
+                    >
+                      <ColorBox
+                        bgcolor="#D7C9E3"
+                        sx={{ flexGrow: 1 }}
+                        title="Hero"
+                      />
+                      <ColorBox
+                        bgcolor="#9FC363"
+                        sx={{ flexGrow: 2 }}
+                        title="Sidebar"
+                      />
+                    </Stack>
+                  </Box>
+                  <Box sx={{ flexGrow: 2, flexBasis: 2 }}>
+                    <Stack
+                      sx={{
+                        height: "100%",
+                        gap: 2,
+                        alignItems: "stretch",
+                      }}
+                    >
+                      <ColorBox
+                        bgcolor="#F5C632"
+                        sx={{ flexGrow: 2 }}
+                        title="Main Content"
+                      />
+                      <ColorBox
+                        bgcolor="#898989"
+                        sx={{ flexGrow: 1 }}
+                        title="Extra Content"
+                      />
+                    </Stack>
+                  </Box>
+                </Stack>
+              </Box>
+              <Box sx={{ flexGrow: 2 }}>
+                <Stack
+                  sx={{
+                    height: "100%",
+                    gap: 2,
+                    alignItems: "stretch",
+                    flexDirection: "row",
+                  }}
+                >
+                  <ColorBox
+                    bgcolor="#2AB676"
+                    sx={{ flexGrow: 2 }}
+                    title="Related Images"
+                  />
+                  <ColorBox
+                    bgcolor="#F3CCDE"
+                    sx={{ flexGrow: 1 }}
+                    title="Related Posts"
+                  />
+                </Stack>
+              </Box>
+              <ColorBox bgcolor="#FEA500" sx={{ flexGrow: 1 }} title="Footer" />
+            </Stack>
+          </Box>
+        </Stack>
       </ThemeProvider>
     </>
   );
